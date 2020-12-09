f42e844c17c463c8298bb8c2d9f905345fc267c2
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-12-09 22:16:17 +0200

    fixed Pnotify

--- src/js/notification.js
+++ src/js/notification.js
@@ -1,8 +1,10 @@
-//  import '@pnotify/core/dist/BrightTheme.css';
-//  import { error } from '@pnotify/core';
- 
-//  const myError = error({
-//    text: "I'm an error message."
-//  });
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import { error } from '@pnotify/core';
 
 
+
+
+const myError = error({
+  text: "I'm an error message."
+});
