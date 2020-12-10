3162a93c93830d4e102cba532efe2c9bea9e4520
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-12-10 21:56:19 +0200

    fixed pnotify theme

--- src/js/notification.js
+++ src/js/notification.js
@@ -2,15 +2,23 @@ import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 import { error } from '@pnotify/core';
 
-function queryError(){
- return error({
-    text: 'Too many matches found. Please enter a more specific query!'})
+function queryError() {
+  return error({
+    text: 'Too many matches found. Please enter a more specific query!',
+    sticker: false,
+    mode: 'light',
+    
+    
+  });
 }
 
-function searchingError(){
-  return  error({
-      text: 'Requested country does not exist. Please, try again!'
-    });
+function searchingError() {
+  return error({
+    text: 'Requested country does not exist. Please, try again!',
+    sticker: false,
+    mode: 'light',
+    
+  });
 }
 
-export {queryError, searchingError}
+export { queryError, searchingError };
